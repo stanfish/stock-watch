d42a851e3a3e9790b18d6475284a5695de080326
Stanley Yu <stanfish@gmail.com>
2019-01-23 14:22:53 -0800

    dayrange picker

---
+++ src/stock/DateRangePicker.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import DayPicker, { DateUtils } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+import './DateRangePicker.css';
+
+export default class DateRangePicker extends React.Component {
+  static defaultProps = {
+    numberOfMonths: 2,
+  };
+  constructor(props) {
+    super(props);
+    this.handleDayClick = this.handleDayClick.bind(this);
+    this.handleResetClick = this.handleResetClick.bind(this);
+    this.state = this.getInitialState();
+  }
+  getInitialState() {
+    return {
+      from: undefined,
+      to: undefined,
+      open: false,
+    };
+  }
+  handleApplyClick = _ => {
+    this.props.onApply(this.state.from, this.state.to);
+    this.setState({open: false});
+  }
+  handleDayClick(day) {
+    const range = DateUtils.addDayToRange(day, this.state);
+    this.setState(range);
+  }
+  handleResetClick() {
+    this.setState(this.getInitialState());
+  }
+  openArea = _ => {
+    this.setState({open: true});
+  }
+
+  render() {
+    const { from, to, open } = this.state;
+    const modifiers = { start: from, end: to };
+
+    const openButton = (!open && 
+      <p>
+        {from &&
+          to &&
+          `Selected from ${from.toLocaleDateString()} to
+              ${to.toLocaleDateString()}`}{' '}        
+        <button className="link" onClick={this.openArea}>
+          Open
+        </button>    
+      </p>
+    );
+
+    const openArea = ( open &&
+      <>
+        <p>
+          {!from && !to && 'Please select the first day.'}
+          {from && !to && 'Please select the last day.'}
+          {from &&
+            to &&
+            `Selected from ${from.toLocaleDateString()} to
+                ${to.toLocaleDateString()}`}{' '}
+        </p>
+        <DayPicker
+          className="Selectable"
+          numberOfMonths={this.props.numberOfMonths}
+          selectedDays={[from, { from, to }]}
+          modifiers={modifiers}
+          onDayClick={this.handleDayClick}
+        />
+
+        {
+          from &&
+            to && (
+              <>
+              <button className="link" onClick={this.handleResetClick}>
+                Reset
+              </button>
+              <button className="link" onClick={this.handleApplyClick}>
+                Apply
+              </button>              
+              </>
+            )
+        }    
+      </>
+    );
+
+    return (
+      <div className="RangeExample">
+        {openButton}
+        {openArea}
+      </div>
+    );
+  }
+}
--- src/stock/StockList.js
+++ src/stock/StockList.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Button, Divider, Loader } from 'semantic-ui-react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import firebase from '../firebase.js';
 import './StockList.css';
-
+import DateRangePicker from './DateRangePicker';
+import 'react-day-picker/lib/style.css';
 class StockList extends Component {
 
   removeItem = itemId => {
@@ -12,9 +14,17 @@ class StockList extends Component {
   }
   
   render() {
+
+    console.log(moment().format('YYYYMMDD'));
+
     return (
       <section className='stock-display-item'>
         <div className="stock-wrapper">
+
+        <DateRangePicker onApply={(from, to) => {
+           console.log('Apply date', moment(from).format('YYYYMMDD'), moment(to).format('YYYYMMDD'));
+        }} />
+
           <ul>
             {this.props.stocks && this.props.stocks.map((item) => {
               return (
