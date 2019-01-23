e7b3edc722a47103f5ba89c880f25d32533cd649
Stanley Yu <stanfish@gmail.com>
2019-01-22 16:13:25 -0800

    separate stock list section from stock page

--- src/stock/StockPage.js
+++ src/stock/StockPage.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import { Button, Divider, Loader } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import firebase from '../firebase.js';
 import './StockPage.css';
 import { setStocks } from '../actions';
 import AddStock from './AddStock';
+import StockList from './StockList';
 
 class StockPage extends Component {
-
-  removeItem = itemId => {
-    const itemRef = firebase.database().ref(`/items/${itemId}`);
-    itemRef.remove();
-  }
 
   componentDidMount() {
     const itemsRef = firebase.database().ref('items');
@@ -34,34 +29,15 @@ class StockPage extends Component {
     return (
         <div className='stock-container'>
           <AddStock />
-          <section className='stock-display-item'>
-              <div className="stock-wrapper">
-                <ul>
-                  {this.props.stocks && this.props.stocks.map((item) => {
-                    return (
-                      <li key={item.id}>
-                        <h3>{item.stock}</h3>
-                            <span>
-                              <Loader inline active={false} />
-                              <Divider section />
-                              <Button secondary onClick={() => this.removeItem(item.id)}>Remove</Button> 
-                            </span>  
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-          </section>
+          <StockList />
         </div>
     );
   }
-
 }
 
 const mapStateToProps = state => {
   return { 
     user: state.user,
-    stocks: state.stocks,
   };
 };
 
