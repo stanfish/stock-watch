acd477d8f97b194a9e539ad6b90b803329178e46
Stanley Yu <stanfish@gmail.com>
2019-01-26 23:38:00 -0800

    fetch stock company

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -26,23 +26,31 @@ const dateRangeReducer = (state=null, action) => {
 }
 
 const stockMapReducer = (stocks={}, action) => {
+  const {stock, price, isFrom, company} = action.payload || {};
   if (action.type === 'FETCH_CURRENT_PRICE') {
-    if (!stocks[action.payload.stock] || !stocks[action.payload.stock].currentPrice) {
+    if (!stocks[stock] || !stocks[stock].currentPrice) {
       let newStocks = {...stocks};
-      newStocks[action.payload.stock] = newStocks[action.payload.stock] || {};
-      newStocks[action.payload.stock].currentPrice = action.payload.price;
+      newStocks[stock] = newStocks[stock] || {};
+      newStocks[stock].currentPrice = price;
       return newStocks;
     }
+  } else if (action.type === 'FETCH_COMPANY') {
+    if (!stocks[stock] || !stocks[stock].company) {
+      let newStocks = {...stocks};
+      newStocks[stock] = newStocks[stock] || {};
+      newStocks[stock].company = company;
+      return newStocks;
+    }    
   } else if (action.type === 'FETCH_DATE_PRICE') {
     let newStocks = {...stocks};
-    let priceKey = action.payload.isFrom ? "fromPrice" : "toPrice";
-    newStocks[action.payload.stock] = newStocks[action.payload.stock] || {};
-    newStocks[action.payload.stock][priceKey] = action.payload.price;
+    let priceKey = isFrom ? "fromPrice" : "toPrice";
+    newStocks[stock] = newStocks[stock] || {};
+    newStocks[stock][priceKey] = price;
 
-    if (newStocks[action.payload.stock] && newStocks[action.payload.stock].fromPrice && newStocks[action.payload.stock].toPrice) {
-      let percentChange = (newStocks[action.payload.stock].toPrice - newStocks[action.payload.stock].fromPrice) / newStocks[action.payload.stock].fromPrice * 100;
+    if (newStocks[stock] && newStocks[stock].fromPrice && newStocks[stock].toPrice) {
+      let percentChange = (newStocks[stock].toPrice - newStocks[stock].fromPrice) / newStocks[stock].fromPrice * 100;
       percentChange = Math.round(percentChange * 1000) / 1000;
-      newStocks[action.payload.stock].percent = percentChange;
+      newStocks[stock].percent = percentChange;
     }
 
     return newStocks;    
