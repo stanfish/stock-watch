a4ee470b490e4194bc4651d77583e7dd1c170609
Stanley Yu <stanfish@gmail.com>
2019-01-30 02:29:15 -0800

    add redux-devtools

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.scss';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware  } from 'redux';
+import { createStore, applyMiddleware, compose  } from 'redux';
 import reducers from './reducers';
 import thunk from 'redux-thunk';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 ReactDOM.render(
-  <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+  <Provider store={createStore(reducers, composeEnhancers(applyMiddleware(thunk)))}>
     <App />
   </Provider>,
   document.getElementById('root')
