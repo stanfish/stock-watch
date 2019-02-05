412719f120b195777fc6ea705d0a3f90a3831260
Stanley Yu <stanfish@gmail.com>
2019-02-04 17:22:28 -0800

    unit test for some actions

--- src/actions/__tests__/index.test.js
+++ src/actions/__tests__/index.test.js
@@ -1,4 +1,4 @@
-import {setUser} from 'actions';
+import {setUser, setStocks, setDateRange, fetchCurrentPrice} from 'actions';
 
 describe('setUser', () => {
   it ('has the correct type', () => {
@@ -11,4 +11,40 @@ describe('setUser', () => {
     expect(action.payload.user).toEqual('new user');
   });
 });
+
+describe('setStocks', () => {
+  it ('has the correct type', () => {
+    const action = setStocks();
+    expect(action.type).toEqual('SET_STOCKS');
+  });
+
+  it ('has the correct payload', () => {
+    const action = setStocks('FB');
+    expect(action.payload.stocks).toEqual('FB');
+  });
+});
+
+describe('setDateRange', () => {
+  it ('has the correct type', () => {
+    const action = setDateRange();
+    expect(action.type).toEqual('SET_DATE_RANGE');
+  });
+
+  it ('has the correct payload', () => {
+    const fromDate = '01-01-2019';
+    const toDate = '02-01-2019';
+    const action = setDateRange(fromDate, toDate);
+    expect(action.payload.fromDate).toEqual(fromDate);
+    expect(action.payload.toDate).toEqual(toDate);
+  });
+});
+
+
+
+//TODO
+//fetchCurrentPrice
+
+//fetchCompany
+
+//fetchDatePrice
 
